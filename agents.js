e829e0f2ffaa4e60d001e032a9d7fac026b21d9c
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-06-03 11:24:01 -0400

    finally dropdown button is functional

--- agents.js
+++ agents.js
@@ -16,7 +16,7 @@ function populateAgentTable(data) {
   });
 }
 
-// Make a GET request to retrieve the agent data
+/* Make a GET request to retrieve the agent data
 fetch("http://99.79.77.144:3000/api/agents")
   .then(response => response.json())
   .then(data => {
@@ -26,3 +26,45 @@ fetch("http://99.79.77.144:3000/api/agents")
     populateAgentTable(filteredAgents);
   })
   .catch(error => console.error("Error:", error));
+*/
+
+  // Function to filter agents by region and update the table
+ // Function to filter agents by region and update the table
+function filterAgentsByRegion(region) {
+  fetch("http://99.79.77.144:3000/api/agents")
+    .then(response => response.json())
+    .then(data => {
+      var filteredAgents = data.filter(agent => agent.rating >= 95);
+
+      if (region !== "all") {
+        filteredAgents = filteredAgents.filter(agent => agent.region.toLowerCase() === region.toLowerCase());
+      }
+
+      populateAgentTable(filteredAgents);
+    })
+    .catch(error => console.error("Error:", error));
+}
+
+// Function to add click event listeners to dropdown menu items
+function addDropdownEventListeners() {
+  var dropdownItems = document.querySelectorAll(".dropdown-item");
+
+  dropdownItems.forEach(item => {
+    item.addEventListener("click", function(event) {
+      event.preventDefault();
+      var region = this.getAttribute("data-region");
+      filterAgentsByRegion(region);
+    });
+  });
+}
+
+// Function to initialize the page and table with all agents
+function initializePage() {
+  filterAgentsByRegion("all");
+  addDropdownEventListeners();
+}
+
+// Call the initializePage function when the page has finished loading
+window.addEventListener("load", initializePage);
+
+
